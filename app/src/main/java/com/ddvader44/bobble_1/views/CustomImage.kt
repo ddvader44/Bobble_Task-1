d3e1e86e63188f31001a3ff13e7c05cd26b442f2
Dishit <40226856+ddvader44@users.noreply.github.com>
2022-02-22 01:38:24 +0530

    completed custom view

--- app/src/main/java/com/ddvader44/bobble_1/views/CustomImage.kt
+++ app/src/main/java/com/ddvader44/bobble_1/views/CustomImage.kt
@@ -13,15 +13,43 @@ class CustomImage @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     private val rect = Rect()
-    private val paint = Paint()
+    private val middleRect = Rect()
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     override fun onDraw(canvas: Canvas) {
+
+        //base rectangle reference
         rect.left = 10
         rect.right = rect.left + 100
         rect.top = 10
-        rect.bottom = rect.right + 100
-        paint.color = Color.GREEN
-        canvas.drawRect(rect,paint)
+        rect.bottom = rect.top + 100
+        paint.color = Color.GRAY
+       // canvas.drawRect(rect,paint)
+
+       //first circle
+
+        val radius = 100f
+
+        val cx = 150f
+        val cy : Float = rect.top.toFloat() + (rect.height())
+       canvas.drawCircle(cx,cy,radius,paint)
+
+        //second circle
+
+        val radius2 = 100f
+        val cx2 = 340f
+        val cy2 : Float = rect.top.toFloat() + (rect.height())
+        canvas.drawCircle(cx2,cy2,radius2,paint)
+
+        //middle rectangle
+
+        middleRect.left = cx.toInt()
+        middleRect.right = cx2.toInt()
+        middleRect.top = (cx - middleRect.height()/2).toInt() + 45
+        middleRect.bottom = (cx2 - middleRect.height()/2).toInt() + 45
+        canvas.drawRect(middleRect,paint)
+
+
     }
 
 }
